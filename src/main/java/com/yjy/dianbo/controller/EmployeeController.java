df053aa83907a1d6bb11c62a6224c68245886796
Junyan Yang <1310204834@qq.com>
2022-08-23 18:56:07 +0800

    2022.8.23

--- src/main/java/com/yjy/dianbo/controller/EmployeeController.java
+++ src/main/java/com/yjy/dianbo/controller/EmployeeController.java
@@ -43,4 +43,9 @@ public class EmployeeController {
         request.getSession().setAttribute("employee", emp.getId());
         return R.success(emp);
     }
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
+    }
 }
