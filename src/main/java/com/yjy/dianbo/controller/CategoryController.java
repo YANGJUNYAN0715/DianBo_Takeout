8eb333a5cfaa43ca6f13d7ebc7bc871807500765
Junyan Yang <1310204834@qq.com>
2022-08-26 22:03:08 +0800

    2022.8.26

--- src/main/java/com/yjy/dianbo/controller/CategoryController.java
+++ src/main/java/com/yjy/dianbo/controller/CategoryController.java
@@ -1,7 +1,11 @@
 package com.yjy.dianbo.controller;
 
+import com.yjy.dianbo.common.R;
+import com.yjy.dianbo.entity.Category;
 import com.yjy.dianbo.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+    @PostMapping
+    public R<String> save(@RequestBody Category category){
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
 }
